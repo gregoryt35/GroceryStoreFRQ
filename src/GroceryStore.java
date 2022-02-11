7fe2db15610ab9225d42ee7a0d66b517002dcc55
BT_1E10_31 <sample@bths.edu>
2022-02-11 10:12:31 -0500

    completed

--- src/GroceryStore.java
+++ src/GroceryStore.java
@@ -7,6 +7,7 @@ public class GroceryStore
      */
     private Product[] productsStocked;
 
+
     public GroceryStore(Product[] initialInventory)
     {
         productsStocked = initialInventory;
@@ -17,7 +18,15 @@ public class GroceryStore
      *  Precondition: min > 0 */
     public ArrayList<Integer> getReorderList(int min)
     {
-        /* to be implemented in part (a) */
+        ArrayList<Integer> returnList = new ArrayList<>();
+
+        for (int i = 0; i < productsStocked.length; i++) {
+            if (productsStocked[i].getQuantity() <= min) {
+                returnList.add(i);
+            }
+        }
+
+        return returnList;
     }
 
     /** Returns true if all products named in shoppingList are available for purchase
@@ -27,6 +36,32 @@ public class GroceryStore
      */
     public boolean checkAvailability(ArrayList<String> shoppingList)
     {
-        /* to be implemented in part (b) */
+        for (int i = 0; i < shoppingList.size(); i++) {
+            for (int j = i + 1; j < productsStocked.length; j++) {
+                if (shoppingList.get(i).equals(productsStocked[j].getName())) {
+                    if (productsStocked[j].getQuantity() == 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
+
+    /** Returns an ArrayList containing all Products from productStocked that
+     *  have a weight that exceeds the “weight” value provided in the parameter
+     */
+    public ArrayList<Product> oversized(double weight)
+    {
+        ArrayList<Product> heavyProducts = new ArrayList<>();
+
+        for (Product prod : productsStocked) {
+            if (prod.getWeight() > weight) {
+                heavyProducts.add(prod);
+            }
+        }
+
+        return heavyProducts;
+    }
+
 }
